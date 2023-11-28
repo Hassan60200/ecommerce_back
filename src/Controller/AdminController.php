fdbcafa78a5cb881ca95416feac4aaf826a6a3e9
Hassan <derkaouihassan19@gmail.com>
2023-11-28 16:05:58 +0100

    create route & function to add new product

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use App\Service\ChartsManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -14,23 +18,42 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 //#[IsGranted("ROLE_ADMIN")]
 class AdminController extends AbstractController
 {
-    public function __construct(private readonly ChartsManager $chartsManager, private readonly ProductRepository $productRepository)
+    public function __construct(private readonly ChartsManager $chartsManager, private readonly ProductRepository $productRepository, private readonly EntityManagerInterface $manager, private readonly CategoryRepository $categoryRepository)
     {
     }
 
     #[Route('/', name: 'app_admin', methods: "GET")]
     public function index(): Response
     {
-      $data = $this->chartsManager->getCountsDatas();
+        $data = $this->chartsManager->getCountsDatas();
 
-       return new JsonResponse($data, 200);
+        return new JsonResponse($data, 200);
     }
 
     #[Route('/products', name: 'app_admin_products', methods: "GET")]
     public function indexProduct(): Response
     {
-        $product= $this->productRepository->findAll();
+        $product = $this->productRepository->findAll();
 
         return new JsonResponse($product, 200);
+    }
+
+    #[Route('/product/new', name: 'admin_add_product', methods: "POST")]
+    public function newProduct(Request $request): Response
+    {
+        $content = json_decode($request->getContent());
+        $product = new Product();
+        $product->setTitle($content->title)
+            ->setDescription($content->description)
+            ->setCategory($this->categoryRepository->find($content->category_id))
+            ->setWeight($content->weight)
+            ->setPrice($content->price)
+            ->setIsBest($content->isBest)
+            ->setIsAvailaible($content->isAvailaible);
+
+        $this->manager->persist($product);
+        $this->manager->flush();
+
+        return new JsonResponse('Un nouveau produit a été ajouté', 200);
     }
 }
